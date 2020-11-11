ffec399b14c77eb0523c4f49fbd8540479182474
DarkSkoll <ericklievana@gmail.com>
2020-11-10 18:43:41 -0600

    selector function

--- Simulator.java
+++ Simulator.java
@@ -10,19 +10,60 @@ public class Simulator{
         this.processes = processes;
         time = 0;
         results = null;
-        algorith = null;
+        algorithm = null;
+    }
+
+    public int getTime(){
+        return time;
     }
 
     public void orderProcessByArriveTime(){
         Process min,tmp;
-        for(int i = 0; i < processes.size(); i++){
-            min = processes.elementAt(i);
-            for(int j = i+1; j < processes.size(); j++){
-                tmp = processes.elementAt(i);
+        int i,j;
+        for(i = 0; i < processes.size(); i++){
+            min = processes.get(i);
+            for(j = i+1; j < processes.size(); j++){
+                tmp = processes.get(i);
                 if(tmp.getArriveTime() < min.getArriveTime()){
                     min = tmp;
                 }
             }
+            tmp = processes.get(i);
+            j = processes.indexOf(min);
+            processes[j] = tmp;
+            processes[i] = min;
+        }
+    }
+
+    public void compute(Process process){
+        time++;
+        process.compute();
+    }
+
+    public void start(){
+        algorithm.run();
+    }
+
+    public void selectAlgorithm(int option){
+        switch(option){
+            case 1:
+                algorithm = new FCFC(this);
+                break;
+            case 2:
+                algorithm = new SJF(this);
+                break;
+            case 3:
+                algorithm = new SRT(this);
+                break;
+            case 4:
+                algorithm = new RoundRobin(this);
+                break;
+            case 5:
+                algorithm = new Priority(this);
+                break;
+            case default:
+                algorithm = null;
+                break;
         }
     }
 }
